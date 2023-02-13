e0507819ebb74fcf55a9be22dfb57c7c7d355d82
Shadow Sketch <blacknihojini@gmail.com>
2023-02-13 17:36:57 +0200

    Create authentication alongside sign in form page

---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,34 @@
+// import { useEffect } from "react";
+// import { getRedirectResult } from 'firebase/auth';
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in/sign-in-form.component";
+
+import './authentication.styles.scss';
+
+const SignIn = () => {
+
+    /*
+    we figured when we leave the page/redirect to another page, the whole instance of our page gets discarded and we lose all auth states
+    called in getRedirectResult() method to aid against this problem - to help retain the state and track app auth state
+    useEffect(() => {
+        async function redirectResponse() {
+            const response = await getRedirectResult(auth);  // method gets auth factor for identification
+            if (response) {
+                const userDocRef = await createUserDocumentFromAuth(response.user);
+            }
+        }
+
+        redirectResponse();
+    }, []);
+    */
+
+    return (
+        <div className="authentication-container">
+            <SignInForm />
+            <SignUpForm />
+        </div>
+    );
+}
+
+export default SignIn;
