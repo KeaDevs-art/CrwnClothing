1429b33452919b47a56f240003dab229ec989021
Shadow Sketch <blacknihojini@gmail.com>
2023-03-04 10:49:53 +0200

    create category routes, update category-item to directory-item to fix clashing styles

---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,24 @@
+import "./directory-item.styles.scss";
+
+// ----------------- imports -------------------------------- imports -------------------------------- imports ---------------
+
+const DirectoryItem = ({ category }) => {
+  const { title, imageUrl } = category;
+
+  return (
+    <div className="directory-item-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="body">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default DirectoryItem;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,7 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
-import { ReactComponent as CrwnLogo } from '../../assets/87 - crown.svg';
+import { ReactComponent as CrwnLogo } from "../../assets/87 - crown.svg";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
@@ -11,40 +11,40 @@ import { CartContext } from "../../contexts/cart.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
-import './navigation.styles.scss';
+import "./navigation.styles.scss";
 
 // ----------------- imports -------------------------------- imports -------------------------------- imports ---------------
 
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
-    const { isCartOpen } = useContext(CartContext);
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
-    return (
-        <Fragment>
-            <div className="navigation">
-                <Link className="logo-container" to="/">
-                    <CrwnLogo className="logo" />
-                </Link>
-                <div className="nav-links-container">
-                    <Link className="nav-link" to="/shop">
-                        SHOP
-                    </Link>
-                    {
-                        currentUser ? (
-                            <span className="nav-link" onClick={signOutUser}>SIGN OUT</span>
-                        ) : (
-                            <Link className="nav-link" to="/auth">
-                                SIGN IN
-                            </Link>
-                        )
-                    }
-                    <CartIcon />
-                </div>
-                {isCartOpen && <CartDropdown />}
-            </div>
-            <Outlet />
-        </Fragment>
-    );
-}
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
+          <CartIcon />
+        </div>
+        {isCartOpen && <CartDropdown />}
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
 
 export default Navigation;
